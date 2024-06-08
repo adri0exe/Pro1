f94998d93be4ce3fb31218908a8f6ba326e1f138
adri0exe <155822431+adri0exe@users.noreply.github.com>
2024-06-08 15:49:46 +0200

    Add files via upload

---
+++ Boletín 1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    char nom[20];
+    char apellidos[30];
+    int edad;
+    int jubilacion;
+
+    //ENTRADA
+    printf("Introduce tu nombre: \t");
+    gets(nom);
+
+    printf("Introduce tus apellidos: \t");
+    gets(apellidos);
+
+    printf("Introduce tu edad: \t");
+    scanf("%d", &edad);
+
+    //PROCESO
+    jubilacion = 67 - edad;
+
+    //SALIDA
+    printf("Sr/Sra. %s, le faltan aun %d años para jubilarse", apellidos, jubilacion);
+    return 0;
+}
